4f107d44eaf9cae3f0e2082d2f516c82e713b40c
Freakcodes <gabhay408@gmail.com>
2023-12-29 18:21:02 +0530

    I hope it works

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Image = () => {
   return (
     <div className='w-[25%] absolute right-0 top-10 mr-[150px] ' >
-        <img src="../images/WhatsApp Image 2023-12-26 at 10.20.32 AM-fotor-20231226103634.jpg" alt="" className=' rounded-full mt-[100px]   ' />
+        <img src="https://drive.google.com/uc?id=1S4oc4Tp5jcj2qgZuOyNy05JWfhzMoIF_" alt="" className=' rounded-full mt-[100px]   ' />
     </div>
   )
 }
