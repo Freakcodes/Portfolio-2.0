06a481f4e26baa83bca0d10788c28c026652b321
Freakcodes <gabhay408@gmail.com>
2024-01-04 13:13:13 +0530

    Modified the header

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Image = () => {
   return (
     <div className='w-[25%] md:absolute md:right-0 md:top-10 md:mr-[150px] absolute top-0 ml-[120px] ' >
-        <img src="https://drive.google.com/uc?id=1S4oc4Tp5jcj2qgZuOyNy05JWfhzMoIF_" alt="" className=' rounded-full mt-[100px]   ' />
+        <img src="https://drive.google.com/uc?id=1r2Vxe2Udod6tS3c7ITDTyJSDZPNGd6Ox" alt="" className=' rounded-full mt-[100px]   ' />
     </div>
   )
 }
