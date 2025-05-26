aed24659485612f69e6874bc5ec0a34fcbf0b421
Freakcodes <gabhay408@gmail.com>
2025-05-26 18:49:10 +0530

    Added avatar

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import myAvatar from '../assets/myAvatar.svg'
 const Image = () => {
   return (
-    <div className='w-[25%] md:absolute md:right-0 md:top-10 md:mr-[150px] absolute top-0 ml-[120px] ' >
-        <img src="https://drive.google.com/uc?id=1r2Vxe2Udod6tS3c7ITDTyJSDZPNGd6Ox" alt="" className=' rounded-full mt-[100px]   ' />
+    <div className=' md:absolute md:right-0 md:top-10 md:mr-[150px] absolute top-0 ml-[120px] w-[300px]  md:flex md:items-center md:h-[calc(100vh-80px)] hidden' >
+        <img src={myAvatar} alt="an Avaatar" className='rounded-full' />
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,9 @@ import Nav from '../components/Nav'
 import {motion as m} from "framer-motion"
 import Icons from '../components/Icons'
 import Image from '../components/Image'
-import CharacterCursor from '../components/CharacterCursor'
+
 import SmoothFollower from '../components/SmoothFollower'
+
 const Home = () => {
   return (
     
@@ -15,22 +16,25 @@ const Home = () => {
     animate={{y:"0%"}}
     transition={{duration:0.75,ease:"easeOut"}}
     exit={{opacity:1}}
-    className=' items-center justify-around h-[100vh] content-center' 
+    className=' items-center justify-around  content-center' 
     >
     
      <SmoothFollower/>
         <Name/>
         <Descrip/>
+        
         <div className="onlyhome mt-4">
         <Nav/>
         </div>
         
         <Icons/>
       
-    
-        {/* <Image /> */}
-      
+      <div className=''>
+          <Image />
+      </div>
         
+      
+       
     </m.div>
     
   )
